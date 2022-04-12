8bac0de4401fb0d5dc6ce393c25c4d10701bd359
Matheus Trevizani Barbosa <matheus.trevizani@hotmail.com>
2022-04-11 22:17:53 -0300

    first commit

---
+++ imagens.js
@@ -0,0 +1,20 @@
+let imagemDaEstrada;
+let imagemDoAtor;
+let imagemDoCarro;
+let imagemDoCarro2;
+let imagemDoCarro3;
+
+//sons
+
+
+
+function preload (){imagemDaEstrada = loadImage ("IMAGENS/estrada.png")
+  imagemDoAtor = loadImage("IMAGENS/ator-1.png")  
+   imagemDoCarro = loadImage ("IMAGENS/carro-1.png")   
+    imagemDoCarro2 = loadImage ("IMAGENS/carro-2.png")
+     imagemDoCarro3 = loadImage ("IMAGENS/carro-3.png")              
+   imagemCarros = [imagemDoCarro, imagemDoCarro2, imagemDoCarro3, imagemDoCarro, imagemDoCarro2, imagemDoCarro3]
+       
+                   
+                   
+                   }
